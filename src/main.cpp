2bcb299422a939ec4bf1c0de275e3f0c3402bc96
Gu11e <guillegomser@gmail.com>
2025-07-08 16:25:36 +0200

    Creación de ventana y priner render de imagen.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
+#include "stb_image.h"
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    int HEIGHT = 600;
+    int WIDTH = 800;
+
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Ventana");
+
+    sf::Texture texture;
+    if (!texture.loadFromFile("../assets/textures/cell.png"))
+    {
+        return -1;
+    }
+
+    sf::Sprite cell;
+    cell.setTexture(texture);
+
+    sf::Vector2u textureSize = texture.getSize();
+
+    cell.setOrigin(textureSize.x / 2.0f, textureSize.y / 2.0f);
+    cell.setPosition(WIDTH/2, HEIGHT/2);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+            if (event.type == sf::Event::Closed) window.close();
+
+        window.clear();
+
+        window.draw(cell);
+
+        window.display();
+    }
+
     return 0;
 }
